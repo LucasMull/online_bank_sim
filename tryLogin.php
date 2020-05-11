f6c0910654f012ed1b95934f7e99bc70d7671b77
LucasMull <muller.lucas@Hotmail.com>
2020-05-11 18:57:31 -0300

    remove space

--- tryLogin.php
+++ tryLogin.php
@@ -1,26 +1,27 @@
 <?php
-require_once "config.php";
+require_once "mysql/config.php";
 
 $agency = trim($_POST['agency']);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $mysqli->query("USE agencies");
         $sql="SELECT * FROM new_$agency WHERE psw = ? AND acc = ?";
         if($stmt = $mysqli->prepare($sql)){
                 $stmt->bind_param("ss", $param_password, $param_account);
                 $param_account = trim($_POST['account']);
                 $param_password = $_POST['password'];
-                if($stmt->execute()) {
+                if($stmt->execute()){
                         $stmt->store_result();
 
-                        if($stmt->num_rows == 1) {
+                        if($stmt->num_rows == 1){
                             header("Location: user.html");
                             exit;
                         } else {
                             echo "Couldn't match password with account or account doesn't exist";
                         }
                 }
-        $stmt->close();
-        exit;
+                $stmt->close();
+                exit;
         }
         $mysqli->close();
 }
